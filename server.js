e6103b84a3fa01883a2aba9dc2c0c69f9c56d20a
truecosmic2025 <admin@truecosmic.com>
2026-05-27 09:05:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const crypto = require('crypto');
+const crypto = require('crypto');  
 
 const app = express();
 const PORT = process.env.PORT || 3000;
